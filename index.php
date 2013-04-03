ee428e1e1e1bac21e203be122438e005c78d8143
Martin Wolf <martin@visuellegedanken.de>
2013-04-03 12:27:12 +0200

    More random numbers and some styling

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="author" content="Martin Wolf">
         
-        <title>CodeThat</title>
+        <title>Give Me Something To Code</title>
         
         <link rel="stylesheet" href="css/style.css">
         
@@ -14,18 +14,26 @@
     <body>
         <div class="page-wrap" ng-controller="getController">
         
-            <h1 class="page-logo">Give Me Something To Code</h1>
-            <button class="btn btn--go" ng-click="getRandom()">Roll the dice again!</button>
+            <header>
+                <h1 class="page-logo">Give Me Something To Code</h1>
+                <button class="btn" ng-click="getRandom()">Roll the dice again!</button>
+            </header>
             
-            <div class="item">
-                <h2>{{dribbbleResult.title}}</h2>
-                <p>By <a href="{{dribbbleResult.player.url}}">{{dribbbleResult.player.name}}</a></p>
-                <img ng-src="{{dribbbleResult.image_url}}" width="{{dribbbleResult.width}}" width="{{dribbbleResult.height}}" alt="{{dribbbleResult.title}}">
-            </div>
+            <main>
+                <div class="item">
+                    <h2 class="item-title">{{dribbbleResult.title}}</h2>
+                    <p class="item-meta">by <a href="{{dribbbleResult.player.url}}">{{dribbbleResult.player.name}}</a></p>
+                    <a href="{{dribbbleResult.url}}">
+                        <img ng-src="{{dribbbleResult.image_url}}" alt="{{dribbbleResult.title}}">
+                    </a>
+                </div>
+                
+                <a class="btn" href="http://codepen.io/pen/" target="_blank">Create a new Pen</a>
+            </main>
             
-            <a class="btn btn--new-pen" href="http://codepen.io/pen/" target="_blank">Create a new Pen</a>
-            
-            <p>Handcrafted by <a href="http://theamazingweb.net">Martin Wolf</a> &amp; powered by the <a href="http://dribbble.com/api">dribbble API</a> and <a href="http://angularjs.org">AngularJS</a>.</p>
+            <footer class="page-footer">
+                <p>Handcrafted by <a href="http://theamazingweb.net">Martin Wolf</a>. Powered by the <a href="http://dribbble.com/api">dribbble API</a> &amp; <a href="http://angularjs.org">AngularJS</a>.</p>
+            </footer>
             
         </div>
         
--- js/app.js
+++ js/app.js
@@ -5,12 +5,13 @@ app.controller('getController', function($scope, $http, getModel) {
     
     $scope.getRandom = function() {
         getModel.getDribbbles($scope);
+        $scope.dribbbleResult.image_url = 'img/loader.gif';
     }
 });
 
 app.service('getModel', function($http) {
     this.getDribbbles = function($scope) {
-        var random = Math.floor(Math.random()*1000);
+        var random = Math.floor(Math.random()*101001);
         var that = this;
         $http(
             {
